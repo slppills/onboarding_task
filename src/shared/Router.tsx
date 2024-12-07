42d4aa9393b96df6dab9cfc58dcc5203535962b4
slppills <sunchaehoon2@naver.com>
2024-12-07 14:39:20 +0900

    feat: router 설정

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,12 +1,19 @@
+import Layout from "@/components/common/Layout";
+import Login from "@/pages/Login";
 import Main from "@/pages/Main";
+import Signup from "@/pages/Signup";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
