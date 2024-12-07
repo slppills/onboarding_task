b11ccc06c4ce471c52b13816accdd80b72e2aec3
slppills <sunchaehoon2@naver.com>
2024-12-08 04:43:02 +0900

    feat: api 요청 함수

---
+++ src/api/auth.ts
@@ -0,0 +1,76 @@
+import { LoginFormdata, SignupFormdata } from "@/types/authType";
+import axios from "axios";
+import { Cookies } from "react-cookie";
+import { NavigateFunction } from "react-router-dom";
+
+const cookies = new Cookies();
+const API_URL = "https://moneyfulpublicpolicy.co.kr";
+
+export const handleRegister = async (formData: SignupFormdata, navigate: NavigateFunction) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, formData);
+    if (response.data.success) {
+      alert("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
+      navigate("/login");
+    }
+  } catch (error) {
+    if (error.response.status === 409) {
+      alert("이미 가입된 아이디입니다.");
+    } else {
+      console.error("회원가입 에러 : ", error);
+    }
+  }
+};
+
+export const handleLogin = async (
+  formData: LoginFormdata,
+  navigate: NavigateFunction,
+  setIsLoggedIn: (loggedIn: boolean) => void
+) => {
+  try {
+    const response = await axios.post(`${API_URL}/login?expiresIn=10m`, formData);
+    if (response.status === 200) {
+      cookies.set("access_token", response.data.accessToken, {
+        path: "/",
+        secure: true,
+        sameSite: "strict",
+      });
+      setIsLoggedIn(true);
+      alert("로그인이 완료되었습니다. 메인페이지로 이동합니다.");
+      navigate("/");
+    }
+  } catch (error) {
+    alert(`로그인 에러 : ${error.response.data.message}`);
+  }
+};
+
+export const getUserInfo = async (token: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/user`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("유저 정보 불러오기 에러 : ", error.response.message);
+  }
+};
+
+export const updateUserInfo = async (token: string, avatar: string, nickname: string) => {
+  try {
+    const response = await axios.patch(
+      `${API_URL}/profile`,
+      { avatar: [avatar], nickname },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("프로필 업데이트 에러 : ", error.response.message);
+  }
+};
