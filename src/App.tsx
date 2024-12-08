61b4b26b3ae338d447d448e7394c9e5c81f5f89e
slppills <sunchaehoon2@naver.com>
2024-12-08 15:18:55 +0900

    add: json placeholder로 외부 통신 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./reset.css";
 import Router from "./shared/Router";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <Router />
       </QueryClientProvider>
     </>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const Main = () => {
-  return <div className=" mt-10">메인페이지입니다. 상단에 페이지로 이동하는 버튼이 있습니다.</div>;
+  const fetchTodos = async () => {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    return data;
+  };
+
+  const { data: todos } = useQuery({
+    queryKey: ["todo", 1],
+    queryFn: fetchTodos,
+    staleTime: 1000 * 5,
+  });
+
+  return (
+    <div className=" mt-10">
+      <h1>메인페이지입니다. 상단에 페이지로 이동하는 버튼이 있습니다.</h1>
+      <p className="mt-5">{todos?.title}</p>
+    </div>
+  );
 };
 
 export default Main;
